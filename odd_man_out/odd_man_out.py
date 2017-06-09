ae4043c637c53cae63ed8baae20c3ebdada77f37
Vindula Jayawardana <vindulamj@gmail.com>
2017-06-09 19:08:45 +0530

    Add Odd man out model

---
+++ odd_man_out/odd_man_out.py
@@ -0,0 +1,32 @@
+from gensim.models.keyedvectors import KeyedVectors
+
+file_log = open("log.txt","a") 
+class_file = open("files/classes","r") 
+instance_file = open("files/instances","r") 
+array = []
+listOfArrays = [] 
+word_list =[]
+
+
+model = KeyedVectors.load_word2vec_format('../text8-vector.bin', binary=True)
+file_log.write("Word2Vec Model loaded\n") 
+print "Word2Vec Model loaded"
+
+for instance in instance_file:
+	for line in class_file:
+		if line.rstrip() == '$$$':
+			listOfArrays.append(array)
+			array = []
+
+		else: 
+        		array.append(line.rstrip())		
+	
+	for arr in listOfArrays:
+		word = ''
+		for i, item in enumerate(arr):
+			if i != 0:
+				word = word +" "+ item
+		file_log.write("Build the word list " + word + "\n") 
+		word_v = model.doesnt_match((word + " " + instance).split())
+		file_log.write("Unmatch word " + word_v + "out of " + word + "\n") 
+		print word_v
