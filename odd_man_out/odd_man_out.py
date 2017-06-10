41ae677a17ad400f394216a58b8ab278b178214f
Vindula Jayawardana <vindulamj@gmail.com>
2017-06-10 07:45:16 +0530

    Add new class creation

--- odd_man_out/odd_man_out.py
+++ odd_man_out/odd_man_out.py
@@ -1,5 +1,6 @@
 from gensim.models.keyedvectors import KeyedVectors
 
+modified_classes = open("files/modified_classes","w") 
 file_log = open("log.txt","a") 
 class_file = open("files/classes","r") 
 instance_file = open("files/instances","r") 
@@ -12,15 +13,15 @@ model = KeyedVectors.load_word2vec_format('../text8-vector.bin', binary=True)
 file_log.write("Word2Vec Model loaded\n") 
 print "Word2Vec Model loaded"
 
-for instance in instance_file:
-	for line in class_file:
-		if line.rstrip() == '$$$':
-			listOfArrays.append(array)
-			array = []
+for line in class_file:
+	if line.rstrip() == '$$$':
+		listOfArrays.append(array)
+		array = []
 
-		else: 
-        		array.append(line.rstrip())		
+	else: 
+        	array.append(line.rstrip())	
 	
+for instance in instance_file:
 	for arr in listOfArrays:
 		word = ''
 		for i, item in enumerate(arr):
@@ -30,3 +31,8 @@ for instance in instance_file:
 		word_v = model.doesnt_match((word + " " + instance).split())
 		file_log.write("Unmatch word " + word_v + "out of " + word + "\n") 
 		print word_v
+		if word_v != instance:
+			arr.append(instance.rstrip())
+		for i, item1 in enumerate(arr):
+			modified_classes.write(item1+"\n")
+		modified_classes.write("$$$\n")
